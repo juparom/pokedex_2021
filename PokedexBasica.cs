5691e67758b434ef4fc2fcbece856a2e7ddc11aa
Jorge Cisneros <CETYSDAM@DESKTOP-HMUGL0C>
2021-04-14 13:46:52 +0200

    finalizado

--- PokedexBasica.cs
+++ PokedexBasica.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PruebaBBDD
 {
@@ -17,14 +18,38 @@ namespace PruebaBBDD
         public PokedexBasica()
         {
             InitializeComponent();
+            asignaPokemon();
+         }
+  
+        private void button2_Click(object sender, EventArgs e)
+        {//boton derecha
+            idActual++;
+            if (idActual > 151) {
+                idActual = 1;
+            }
+            asignaPokemon();
         }
 
-        //boton derecha
-        private void button2_Click(object sender, EventArgs e)
+
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            idActual++;
+            idActual--;
+            if (idActual < 1)
+            {
+                idActual = 151;
+            }
+            asignaPokemon();
+        }
+
+        private void asignaPokemon() {
             DataTable pokemonElegido = miConexion.getPokemonPorId(idActual);
             nombrePokemon.Text = pokemonElegido.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlobAImagen((byte[])pokemonElegido.Rows[0]["imagen"]);
+        }
+
+        private Image convierteBlobAImagen(byte[] img) {
+            return (Image.FromStream(new System.IO.MemoryStream(img)));
         }
     }
 }
