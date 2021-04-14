d248e42c188f52e2f153bed427d7957870a7b0c9
Jorge Cisneros <CETYSDAM@DESKTOP-HMUGL0C>
2021-04-14 12:07:58 +0200

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;  //la libreria de MySql
+using System.Data;  //la libreria del DataTable
+
+namespace PruebaBBDD
+{
+    class Conexion
+    {
+        public MySqlConnection conexion; //variable que se encarga de conectarnos al servidor MySql
+
+        public Conexion() { //el constructor de la clase
+            conexion = new MySqlConnection("Server=127.0.0.1; Database=listapokemons; Uid=root; Pwd=; Port=3306 ");
+        }
+
+        public DataTable getPokemons() {
+            try {
+                conexion.Open();
+                MySqlCommand consulta = new MySqlCommand("SELECT * FROM pokemon", conexion);
+                MySqlDataReader resultado = consulta.ExecuteReader(); //guardo el resultado de la query
+                DataTable pokemons = new DataTable(); //formato que espera el datagridview
+                pokemons.Load(resultado);  //convierte MysqlDataReader en DataTable
+                conexion.Close();
+                return pokemons;
+            }
+            catch (MySqlException e) {
+                throw e;
+            }
+
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaBBDD
+{
+    public partial class Form1 : Form
+    {
+        Conexion miConexion = new Conexion(); //esta variable es del tipo que hemos creado
+                                                //para conectarnos a la BBDD MySql
+
+        DataTable misPokemons = new DataTable(); //variable que almacena el array de dos 
+                                                   //dimensiones resultado de la consulta
+        public Form1()
+        {
+            InitializeComponent();
+            misPokemons = miConexion.getPokemons();
+            dataGridView1.DataSource = misPokemons;
+        }
+    }
+}
